c351fa225918688af7bc4ad6f60c21cf71a66641
AliCormack <abgc500@york.ac.uk>
2017-04-17 16:11:50 +0100

    Added iWave algorithm. Added Convolve function for applying kernel.

--- Water Wakes/Assets/Scripts/WaterWakes.cs
+++ Water Wakes/Assets/Scripts/WaterWakes.cs
@@ -21,7 +21,21 @@ public class WaterWakes : MonoBehaviour {
     // Store precomputed kernel values
     float[,] storedKernelArray;
 
-	void Start () {
+    Vector3[][] height;
+    Vector3[][] previousHeight;
+    Vector3[][] verticalDerivative;
+    public Vector3[][] source;
+    public Vector3[][] obstruction;
+    // 1d array for updating mesh
+    public Vector3[] unfolded_verts;
+    // Ambient Waves
+    public Vector3[][] heightDifference;
+    // Calculate once
+    int arrayLength;
+
+    float updateTimer = 0f;
+
+    void Start () {
 
         waterMeshFilter = this.GetComponent<MeshFilter>();
 
@@ -39,7 +53,184 @@ public class WaterWakes : MonoBehaviour {
         storedKernelArray = new float[P * 2 + 1, P * 2 + 1];
         PrecomputeKernelValues();
 
-	}
+        height = height_tmp.ToArray();
+        //Need to clone these
+        previousHeight = CloneList(height);
+        verticalDerivative = CloneList(height);
+        source = CloneList(height);
+        obstruction = CloneList(height);
+        heightDifference = CloneList(height);
+
+        //Create this once here, so we dont need to create it each update
+        unfolded_verts = new Vector3[height.Length * height.Length];
+        arrayLength = height.Length;
+
+        //Add obstruction when the wave hits the walls
+        for ( int j = 0; j < arrayLength; j++ )
+        {
+            for ( int i = 0; i < arrayLength; i++ )
+            {
+                if ( j == 0 || j == arrayLength - 1 || i == 0 || i == arrayLength - 1 )
+                {
+                    obstruction[j][i].y = 0f;
+                }
+                else
+                {
+                    obstruction[j][i].y = 1f;
+                }
+            }
+        }
+
+    }
+
+    void Update()
+    {
+        updateTimer += Time.deltaTime;
+
+        if ( updateTimer > 0.02f)
+        {
+            MoveWater(0.02f);
+            updateTimer = 0;
+        }
+    }
+
+    void MoveWater(float dt)
+    {
+        AddWaterWakes(dt);
+
+        // Update Mesh
+        for(int i = 0; i<arrayLength; i++)
+        {
+            heightDifference[i].CopyTo(unfolded_verts, i * heightDifference.Length);
+        }
+
+        waterMesh.vertices = unfolded_verts;
+        waterMesh.RecalculateBounds();
+        waterMesh.RecalculateNormals();
+    }
+
+    void AddWaterWakes(float dt)
+    {
+        // Add sources and obstructions
+        for ( int j = 0; j < arrayLength; j++ )
+        {
+            for ( int i = 0; i < arrayLength; i++ )
+            {
+                height[j][i].y += source[j][i].y;
+                source[j][i].y = 0f;
+                height[j][i].y *= obstruction[j][i].y;
+            }
+        }
+
+        // Convolve to update vertical derivative
+        Convolve();
+
+        float twoMinusAlphaTimesDt = 2f - alpha * dt;
+        float onePlusAlphaTimesDt = 1f + alpha * dt;
+        float gravityTimesDtTimesDt = g * dt * dt;
+
+        for ( int j = 0; j < arrayLength; j++ )
+        {
+            for ( int i = 0; i < arrayLength; i++ )
+            {
+                float currentHeight = height[j][i].y;
+
+                float newHeight = 0f;
+
+                newHeight += currentHeight * twoMinusAlphaTimesDt;
+                newHeight -= previousHeight[j][i].y;
+                newHeight -= gravityTimesDtTimesDt * verticalDerivative[j][i].y;
+                newHeight /= onePlusAlphaTimesDt;
+
+                previousHeight[j][i].y = currentHeight;
+
+                height[j][i].y = newHeight;
+
+                // If we want ambient waves add here
+                // Replace with a call to a method where you find the current height of the ambient wave
+                // at the current coord
+                float heightAmbientWave = 0f;
+
+                heightDifference[j][i].y = heightAmbientWave + newHeight;
+            }
+        }
+    }
+
+    // Convolve height with the kernel and put it into vertical_derivative
+    // Loop through all verticles in a square with dimension (-P, P)
+    // aroudn current vertice. Similar to kernel image processing.
+    // Also have to take into account what will happen if a section of this square is outside water mesh.
+    // Currently ignores corner values.
+
+    // This is a slower implementation. Faster one can be found here
+    // http://jtessen.people.clemson.edu/papers_files/Interactive_Water_Surfaces.pdf
+    void Convolve()
+    {
+        for ( int j = 0; j < arrayLength; j++ )
+        {
+            for ( int i = 0; i < arrayLength; i++ )
+            {
+                float vDeriv = 0f;
+
+                for ( int k = -P; k <= P; k++ )
+                {
+                    for ( int l = -P; l <= P; l++ )
+                    {
+                        // Get precomputed
+                        float kernelValue = storedKernelArray[k + P, l + P];
+
+                        // Check within water
+                        if ( j + k >= 0 && j + k < arrayLength && i + l >= 0 && i+l < arrayLength)
+                        {
+                            vDeriv += kernelValue * height[j + k][i + l].y;
+                        }   
+                        //Outside
+                        else
+                        {
+                            //Right
+                            if ( j + k >= arrayLength && i + l >= 0 && i + l < arrayLength )
+                            {
+                                vDeriv += kernelValue * height[2 * arrayLength - j - k - 1][i + l].y;
+                            }
+                            //Top
+                            else if ( i + l >= arrayLength && j + k >= 0 && j + k < arrayLength )
+                            {
+                                vDeriv += kernelValue * height[j + k][2 * arrayLength - i - l - 1].y;
+                            }
+                            //Left
+                            else if ( j + k < 0 && i + l >= 0 && i + l < arrayLength )
+                            {
+                                vDeriv += kernelValue * height[-j - k][i + l].y;
+                            }
+                            //Bottom
+                            else if ( i + l < 0 && j + k >= 0 && j + k < arrayLength )
+                            {
+                                vDeriv += kernelValue * height[j + k][-i - l].y;
+                            }
+                        }
+                    }
+                }
+
+                verticalDerivative[j][i].y = vDeriv;
+
+            }
+        }
+    }
+
+    //Clone an array and the inner array
+    Vector3[][] CloneList(Vector3[][] arrayToClone)
+    {
+        //First clone the outer array
+        Vector3[][] newArray = arrayToClone.Clone() as Vector3[][];
+
+        //Then clone the inner arrays
+        for ( int i = 0; i < newArray.Length; i++ )
+        {
+            newArray[i] = newArray[i].Clone() as Vector3[];
+        }
+
+        return newArray;
+    }
 
     // Using the iWave algorithm for surface tension
     // http://jtessen.people.clemson.edu/papers_files/Interactive_Water_Surfaces.pdf
